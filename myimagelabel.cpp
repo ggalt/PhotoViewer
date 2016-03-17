9b6ac753acbc432b3ca0c221c6baa031949abf9c
George Galt <george@galtfamily.net>
2016-03-17 18:06:10 -0400

    More work on custom label

--- myimagelabel.cpp
+++ myimagelabel.cpp
@@ -1,28 +1,137 @@
 #include "myimagelabel.h"
 
-myImageLabel::myImageLabel(QObject *parent) : QLabel(parent)
+myImageLabel::myImageLabel(QWidget *parent) : QLabel(parent)
 {
 
 }
 
-void myImageLabel::init(QImage *one = NULL, QImage *two = NULL)
+void myImageLabel::init(QImage *one, QImage *two)
 {
     tick = 0;
-    originalImage[0] = one;
-    originalImage[1] = two;
+    originalImage[NEW] = one;
+    originalImage[OLD] = two;
+    timeLine = new QTimeLine(2000,this);
+    timeLine->setFrameRange(0,254);
+    connect(timeLine, SIGNAL(frameChanged(int)),
+            this, SLOT(animationStep(int)));
+    createImages();
 }
 
 void myImageLabel::nextImage(QImage *next)
 {
+//    originalImage[(tick&1)] = originalImage[((tick+1)&1)];
+//    originalImage[(tick&1)] = next;
+    originalImage[OLD] = originalImage[NEW];
+    originalImage[NEW] = next;
 
+//    tick++;
+
+    createImages();
 }
 
 void myImageLabel::animationStep(int val)
 {
+    // effectStep should be between 0 -> 254
+    if( val > 254 )
+        effectStep = 254;
+    else if( val < 0 )
+        effectStep = 0;
+    else
+        effectStep = val;
+}
 
+void myImageLabel::createImages(void)
+{
+    foregroundImage[OLD] = QPixmap::fromImage(
+                originalImage[OLD]->scaled(this->size(),Qt::KeepAspectRatio));
+    foregroundImage[NEW] = QPixmap::fromImage(
+                originalImage[NEW]->scaled(this->size(),Qt::KeepAspectRatio));
+
+    // background images always contain a blur, so blur the base image so we only do it once
+    QGraphicsBlurEffect *blur[2];
+    blur[OLD] = new QGraphicsBlurEffect();
+    blur[NEW] = new QGraphicsBlurEffect();
+    blur[OLD]->setBlurRadius(blurValue);
+    blur[NEW]->setBlurRadius(blurValue);
+    backgroundImage[OLD] = QPixmap::fromImage(
+                applyEffectToImage(
+                    QPixmap::fromImage( originalImage[OLD]->scaled(
+                        this->size(),Qt::KeepAspectRatioByExpanding)),blur[OLD]));
+    backgroundImage[NEW] = QPixmap::fromImage(
+                applyEffectToImage(
+                    QPixmap::fromImage( originalImage[NEW]->scaled(
+                        this->size(),Qt::KeepAspectRatioByExpanding)),blur[NEW]));
 }
 
 void myImageLabel::paintCompositeImage(void)
 {
+    QGraphicsScene scene;
+    QGraphicsPixmapItem foreGround[2];
+    QGraphicsPixmapItem backGround[2];
 
+    QImage composite(this->size(), QImage::Format_ARGB32);
+    composite.fill(Qt::transparent);
+
+    if(backgroundEffect == Blur) {
+        // NOTE: Blur effect only uses the first QGraphcisPixmapItem
+        QGraphicsBlurEffect *blur = new QGraphicsBlurEffect;
+        if( effectStep < 127 ) { // blurring out
+            blur->setBlurRadius(effectStep * 2);
+            backGround[OLD].setPixmap(backgroundImage[OLD]);
+            backGround[OLD].setGraphicsEffect(blur);
+            scene.addItem(&backGround[OLD]);
+        } else {            // blurring back in
+            blur->setBlurRadius((254 - effectStep) * 2);
+            backGround[OLD].setPixmap(backgroundImage[NEW]);
+            backGround[OLD].setGraphicsEffect(blur);
+            scene.addItem(&backGround[OLD]);
+        }
+    }
+    else if( backgroundEffect == Dissolve ) {
+        QGraphicsOpacityEffect *opacity[2];
+        opacity[OLD] = new QGraphicsOpacityEffect();
+        opacity[NEW] = new QGraphicsOpacityEffect();
+        qreal op = (1/254)*(qreal)effectStep;
+        opacity[OLD]->setOpacity((qreal)1.0 - op);
+        opacity[NEW]->setOpacity(op);
+        backGround[OLD].setPixmap(backgroundImage[OLD]);
+        backGround[OLD].setGraphicsEffect(opacity[OLD]);
+        backGround[NEW].setPixmap(backgroundImage[NEW],blur);
+        backGround[NEW].setGraphicsEffect(opacity[NEW]);
+        scene.addItem(&backGround[OLD]);
+        scene.addItem(&backGround[NEW]);
+    } else {
+        scene.addPixmap(backgroundImage[NEW]);
+    }
+
+    if( foregroundEffect == Blur ) {
+
+    }
+    else if( foregroundEffect == Dissolve ) {
+
+    } else {
+
+    }
+
+    QPainter p(&composite);
+    scene.render(&p, QRectF(), QRectF(0,0,this->width(),this->height()));
+    this->setPixmap(QPixmap::fromImage(composite));
 }
+
+QImage myImageLabel::applyEffectToImage(QPixmap src, QGraphicsEffect *effect, int extent)
+{
+    if(src.isNull())
+        return QImage();   //No need to do anything else!
+
+    QGraphicsScene scene;
+    QGraphicsPixmapItem item;
+    item.setPixmap(src);
+    item.setGraphicsEffect(effect);
+    scene.addItem(&item);
+    QImage res(src.size()+QSize(extent*2, extent*2), QImage::Format_ARGB32);
+    res.fill(Qt::transparent);
+    QPainter ptr(&res);
+    scene.render(&ptr, QRectF(), QRectF( -extent, -extent, src.width()+extent*2, src.height()+extent*2 ) );
+    return res;
+}
+
--- myimagelabel.h
+++ myimagelabel.h
@@ -2,21 +2,37 @@
 #define MYIMAGELABEL_H
 
 #include <QObject>
+#include <QWidget>
 #include <QLabel>
 
 #include <QPainter>
 #include <QImage>
 #include <QPixmap>
+#include <QGraphicsPixmapItem>
+#include <QGraphicsScene>
+#include <QGraphicsBlurEffect>
+#include <QGraphicsOpacityEffect>
+#include <QRectF>
 
 #include <QTimeLine>
+
+enum EFFECT{
+    NoEffect = 0,
+    Dissolve,
+    Blur
+};
+
+#define NEW 0
+#define OLD 1
 
 class myImageLabel : public QLabel
 {
     Q_OBJECT
 public:
-    explicit myImageLabel(QObject *parent = 0);
+    explicit myImageLabel(QWidget *parent = 0);
     void init(QImage *one = NULL, QImage *two = NULL);
     void nextImage(QImage *next);
+    void setBlurValue(int b) { blurValue = b; }
 
 signals:
 
@@ -25,14 +41,19 @@ public slots:
 
 private:
     void paintCompositeImage(void);
+    void createImages(void);
+    QImage applyEffectToImage(QPixmap src, QGraphicsEffect *effect, int extent = 0);
 
 private:
     QImage *originalImage[2];
-    QImage *foregroundImage[2];
-    QImage *backgroundImage[2];
-    QImage *composite;
-    QPainter *p;
+    QPixmap foregroundImage[2];
+    QPixmap backgroundImage[2];
+
+    EFFECT foregroundEffect;
+    EFFECT backgroundEffect;
+    int blurValue;          // base blur value of background
     QTimeLine *timeLine;
+    int effectStep;
     int tick;       // read value with (tick & 1) to get the last bit value (0 or 1)
 };
 
